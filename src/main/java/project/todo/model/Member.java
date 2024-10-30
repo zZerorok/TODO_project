dcbcf9210804099cc5cb401e77b920866858157f
youngrok <yr951103@gmail.com>
2024-10-30 19:45:32 +0900

    Feat: Member 클래스 Entity 클래스로 설정
    - @Entity, @Id 등 어노테이션 추가
    - id 필드 기본형 long -> 래퍼 클래스 Long 변경
    - 기본 생성자 추가
    - getter 메서드 추가

--- src/main/java/project/todo/model/Member.java
+++ src/main/java/project/todo/model/Member.java
@@ -1,10 +1,30 @@
 package project.todo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String name;
+
+    public Member() {
+    }
 
     public Member(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 }
