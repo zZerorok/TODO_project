a04af1788ea276b3515410ea37b71d4b83909097
youngrok <yr951103@gmail.com>
2024-10-31 20:53:17 +0900

    Feat: MemberService 생성
    - Member 관련 비즈니스 로직을 담당할 서비스 클래스 생성
    - @Service 어노테이션 추가
    - 생성자를 통해 MemberRepository 의존성 주입

---
+++ src/main/java/project/todo/model/MemberService.java
@@ -0,0 +1,13 @@
+package project.todo.model;
+
+import org.springframework.stereotype.Service;
+import project.todo.model.repository.MemberRepository;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+}
