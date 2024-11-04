0018fb3c5f74a000ef831aebf6e5ed4d376a9362
youngrok <yr951103@gmail.com>
2024-11-04 15:29:46 +0900

    Feat: HTTP 요청 분기 처리 추가
    - POST 요청으로 사용자의 이름을 받아 Member 객체를 생성하고 저장
    - /hello 경로에 대한 GET 요청을 통해 환영 페이지를 반환

--- src/main/java/project/todo/controller/MemberController.java
+++ src/main/java/project/todo/controller/MemberController.java
@@ -1,6 +1,10 @@
 package project.todo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import project.todo.model.Member;
 import project.todo.model.MemberService;
 
 @Controller
@@ -9,5 +13,18 @@ public class MemberController {
 
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PostMapping("/hello")
+    public String save(String memberName, Model model) {
+        Member member = new Member(memberName);
+        memberService.save(member);
+        model.addAttribute("member", member);
+        return "hello";
     }
 }
