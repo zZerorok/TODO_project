e843c73ca8eb59bdb16372e6df8a5cfe13cf7f67
youngrok <yr951103@gmail.com>
2024-11-20 14:50:55 +0900

    Refactor: MemberController 구조 개선
    - /member 경로를 추가하여 향후 작업 확장에 대비
    - MemberCreateRequest를 @RequestBody로 받아 JSON 형식으로 데이터를 처리하도록 수정

--- src/main/java/project/todo/controller/MemberController.java
+++ src/main/java/project/todo/controller/MemberController.java
@@ -2,13 +2,12 @@ package project.todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import project.todo.model.member.Member;
 import project.todo.model.member.MemberCreateRequest;
 import project.todo.service.MemberService;
 
+@RequestMapping("/member")
 @Controller
 public class MemberController {
     private final MemberService memberService;
@@ -24,7 +23,7 @@ public class MemberController {
     }
 
     @PostMapping("/hello")
-    public String save(MemberCreateRequest request) {
+    public String save(@RequestBody MemberCreateRequest request) {
         Member savedMember = memberService.save(request);
         return "redirect:/hello?name=" + savedMember.getName();
     }
