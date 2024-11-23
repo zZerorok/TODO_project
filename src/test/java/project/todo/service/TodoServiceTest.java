00cde8df9a762b2ac2396c3f8501dbc269e52d0a
youngrok <yr951103@gmail.com>
2024-11-23 20:11:55 +0900

    Fix: 클래스 단위 트랜잭션 처리 추가로 전체 테스트 통과

--- src/test/java/project/todo/service/TodoServiceTest.java
+++ src/test/java/project/todo/service/TodoServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import project.todo.model.member.Member;
 import project.todo.model.member.MemberRepository;
 import project.todo.model.todo.Todo;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @SpringBootTest
 class TodoServiceTest {
 
