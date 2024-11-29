29429cab4d18cadc3db09f2ed19a6f8a5bced6b9
youngrok <yr951103@gmail.com>
2024-11-29 21:54:51 +0900

    test: Task 관련 예외 테스트 추가 및 @DisplayName 추가

--- src/test/java/project/todo/service/TaskServiceTest.java
+++ src/test/java/project/todo/service/TaskServiceTest.java
@@ -1,6 +1,8 @@
 package project.todo.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +12,7 @@ import project.todo.model.member.MemberRepository;
 import project.todo.model.todo.Todo;
 import project.todo.model.todo.TodoRepository;
 import project.todo.model.todo.task.Task;
+import project.todo.model.todo.task.TaskAddRequest;
 import project.todo.model.todo.task.TaskRepository;
 import project.todo.model.todo.task.TaskResponse;
 
@@ -17,6 +20,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
@@ -55,40 +59,47 @@ class TaskServiceTest {
         taskRepository.saveAll(tasks);
     }
 
-    @Test
-    void addTaskTest() {
-        Todo todo = todoRepository.findAll().get(0);
-        Task task = new Task(
-                todo,
-                "task1"
-        );
-        Task savedTask = taskRepository.save(task);
-
-        assertThat(savedTask).isNotNull();
-        assertThat(savedTask.getContent()).isEqualTo("task1");
-    }
-
+    @DisplayName("Todo에 포함된 Task 전체 조회")
     @Test
     void findTasksTest() {
-        Todo todo = todoRepository.findAll().get(0);
+        Long todoId = todoRepository.findAll().get(0).getId();
 
-        List<TaskResponse> tasks = taskService.findTasks(todo.getId());
+        List<TaskResponse> tasks = taskService.findTasks(todoId);
 
-        assertThat(tasks).isNotNull();
         assertThat(tasks).hasSize(3);
     }
 
+    @DisplayName("Todo에 포함된 Task 전체 조회 시 Task가 존재하지 않을 경우 예외 발생")
     @Test
-    void deleteTaskTest() {
-        Task task = taskRepository.findAll().get(0);
-        Long taskId = task.getId();
+    void findNoTaskTest() {
+        taskRepository.deleteAll();
+        Long todoId = todoRepository.findAll().get(0).getId();
 
-        taskService.delete(taskId);
-
-        assertThat(taskRepository.findById(taskId)).isEmpty();
-        assertThat(taskRepository.findAll()).hasSize(2);
+        assertThatThrownBy(() -> {
+            taskService.findTasks(todoId);
+        })
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("해당 Todo에 대한 Task가 존재하지 않습니다.");
     }
 
+    @DisplayName("Todo에 새로운 Task 추가")
+    @Test
+    void addTaskTest() {
+        Long todoId = todoRepository.findAll().get(0).getId();
+        TaskAddRequest task = new TaskAddRequest("task1");
+        taskService.add(todoId, task);
+
+        Task addedTask = taskRepository.findAll().stream()
+                .filter(it -> it.getContent().equals("task1"))
+                .findFirst()
+                .orElseThrow();
+
+        assertNotNull(addedTask);
+        assertEquals(addedTask.getTodo().getId(), todoId);
+        assertEquals(addedTask.getContent(), task.content());
+    }
+
+    @DisplayName("Task 완료 처리")
     @Test
     void completeTaskTest() {
         Task task = taskRepository.findAll().get(0);
@@ -97,9 +108,44 @@ class TaskServiceTest {
         assertFalse(task.isCompleted());
 
         taskService.completeTask(taskId);
-
         Task completedTask = taskRepository.findById(taskId).orElseThrow();
         assertNotNull(completedTask.getCompletedAt());
         assertTrue(completedTask.isCompleted());
+    }
+
+    @DisplayName("이미 완료된 Task 에외 발생 ")
+    @Test
+    void alreadyCompletedTaskTest() {
+        Long taskId = taskRepository.findAll().get(0).getId();
+        taskService.completeTask(taskId);
+
+        assertThatThrownBy(() -> {
+            taskService.completeTask(taskId);
+        })
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 완료된 Task입니다.");
+    }
+
+    @DisplayName("Task 삭제 처리")
+    @Test
+    void deleteTaskTest() {
+        Long taskId = taskRepository.findAll().get(0).getId();
+
+        taskService.delete(taskId);
+
+        assertThat(taskRepository.findById(taskId)).isEmpty();
+        assertThat(taskRepository.findAll()).hasSize(2);
+    }
+
+    @DisplayName("Task 삭제 시 Task가 존재하지 않으면 예외 발생")
+    @Test
+    void deleteNoTaskTest() {
+        Long invalidTaskId = 999L;
+
+        assertThatThrownBy(() -> {
+            taskService.delete(invalidTaskId);
+        })
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("해당 Task를 찾을 수 없습니다.");
     }
 }
