67f3965b730582e3f5790da8ef3bc46630562f00
youngrok <yr951103@gmail.com>
2024-12-08 18:15:46 +0900

    test: Member 도메인 테스트 코드 개선

--- src/test/java/project/todo/model/MemberTest.java
+++ src/test/java/project/todo/model/MemberTest.java
@@ -1,6 +1,5 @@
 package project.todo.model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,20 +7,27 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import project.todo.model.member.Member;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class MemberTest {
 
-    @NullAndEmptySource
     @ValueSource(strings = {" ", "   "})
-    @ParameterizedTest(name = "{0} 입력하면 예외를 던진다.")
-    void invalidNameSpace(String invalidName) {
-        Assertions.assertThatThrownBy(() -> new Member(invalidName))
-                .isInstanceOf(IllegalArgumentException.class);
+    @NullAndEmptySource
+    @ParameterizedTest(name = "객체 생성 시 빈 문자열 또는 공백이면 예외 발생")
+    void initMemberWithEmptyName(String name) {
+
+        assertThatThrownBy(() -> new Member(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름을 입력해주세요.");
     }
 
+
+    @DisplayName("객체 생성 시 이름이 10자를 초과하면 예외 발생")
     @Test
-    @DisplayName("사용자의 이름은 10글자를 넘어갈 수 없다.")
-    void invalidNameLength() {
-        Assertions.assertThatThrownBy(() -> new Member("A".repeat(11)))
-                .isInstanceOf(IllegalArgumentException.class);
+    void initMemberWithExceedingLength() {
+
+        assertThatThrownBy(() -> new Member("A".repeat(11)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 10자를 초과할 수 없습니다.");
     }
 }
