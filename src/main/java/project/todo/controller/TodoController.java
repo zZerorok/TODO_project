fb39ef6750aef981a6853bf0068b346d9d76cc1d
youngrok <yr951103@gmail.com>
2024-12-09 17:58:28 +0900

    refactor: TodoController RESTful하게 수정
    - @Controller -> @RestController로 변경하여 자동 응답 처리
    - 메서드 URL 및 HTTP 메서드 일관성 있게 정리
    - 불필요한 /create, /delete URL 제거

--- src/main/java/project/todo/controller/TodoController.java
+++ src/main/java/project/todo/controller/TodoController.java
@@ -2,7 +2,6 @@ package project.todo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import project.todo.model.todo.TodoCreateRequest;
 import project.todo.model.todo.TodoResponse;
@@ -12,7 +11,7 @@ import project.todo.service.TodoService;
 import java.util.List;
 
 @RequestMapping("/todos")
-@Controller
+@RestController
 public class TodoController {
     private final TodoService todoService;
 
@@ -21,13 +20,15 @@ public class TodoController {
     }
 
     @GetMapping("/{memberId}")
-    public List<TodoResponse> findAll(@PathVariable Long memberId) {
-        return todoService.findAll(memberId);
+    public ResponseEntity<List<TodoResponse>> findTodos(@PathVariable Long memberId) {
+        List<TodoResponse> todos = todoService.findTodos(memberId);
+        return ResponseEntity.ok(todos);
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Void> create(@RequestBody TodoCreateRequest request) {
-        todoService.create(request);
+    @PostMapping("/{memberId}")
+    public ResponseEntity<Void> create(@PathVariable Long memberId,
+                                       @RequestBody TodoCreateRequest request) {
+        todoService.create(memberId, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
@@ -35,19 +36,19 @@ public class TodoController {
     public ResponseEntity<Void> updateAll(@PathVariable Long todoId,
                                           @RequestBody TodoUpdateRequest request) {
         todoService.updateAll(todoId, request);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/{todoId}")
+    @PatchMapping("/{todoId}")
     public ResponseEntity<Void> update(@PathVariable Long todoId,
                                        @RequestBody TodoUpdateRequest request) {
         todoService.update(todoId, request);
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.ok().build();
     }
 
-
-    @DeleteMapping("/{todoId}/delete")
-    public void delete(@PathVariable Long todoId) {
+    @DeleteMapping("/{todoId}")
+    public ResponseEntity<Void> delete(@PathVariable Long todoId) {
         todoService.delete(todoId);
+        return ResponseEntity.noContent().build();
     }
 }
