8fe5c95551624da79e1b994caf762010b98c335b
youngrok <yr951103@gmail.com>
2024-12-12 00:54:40 +0900

    refactor: TaskUpdateRequest - Task 수정 및 검증 로직을 책임지도록 변경

--- src/main/java/project/todo/model/todo/task/TaskUpdateRequest.java
+++ src/main/java/project/todo/model/todo/task/TaskUpdateRequest.java
@@ -3,4 +3,21 @@ package project.todo.model.todo.task;
 public record TaskUpdateRequest(
         String content
 ) {
+    public void updateTo(Task task) {
+        validateComplete(task);
+
+        if (content != null) {
+            task.changeContent(content);
+        }
+    }
+
+    private void validateComplete(Task task) {
+        if (task.isCompleted()) {
+            throw new IllegalArgumentException("이미 완료된 Task는 수정할 수 없습니다.");
+        }
+    }
+
+    public boolean isChanged(Task task) {
+        return !content.equals(task.getContent());
+    }
 }
