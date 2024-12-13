94d0e9bd49ee316678a7a77e0da2688e113ced4c
youngrok <yr951103@gmail.com>
2024-12-13 19:13:48 +0900

    feat: TaskRepository - TodoId에 속한 Task 삭제 쿼리 메서드 추가

--- src/main/java/project/todo/repository/todo/task/TaskRepository.java
+++ src/main/java/project/todo/repository/todo/task/TaskRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     List<Task> findAllByTodoId(Long todoId);
+
+    void deleteByTodoId(Long todoId);
 }
