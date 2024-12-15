9b98f21d884d93cffcab3c984092f460474ad042
youngrok <yr951103@gmail.com>
2024-12-15 18:23:48 +0900

    refactor: TaskController - 서비스 로직 분리에 따른 코드 수정
    - Lombok 어노테이션(@RequiredArgsConstructor)으로 생성자 추가

--- src/main/java/project/todo/controller/TaskController.java
+++ src/main/java/project/todo/controller/TaskController.java
@@ -1,54 +1,54 @@
 package project.todo.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import project.todo.service.todo.task.TaskAddRequest;
-import project.todo.service.todo.task.TaskResponse;
-import project.todo.service.todo.task.TaskUpdateRequest;
-import project.todo.service.todo.task.TaskService;
+import project.todo.service.todo.task.TaskWriteService;
+import project.todo.service.todo.task.dto.TaskAddRequest;
+import project.todo.service.todo.task.dto.TaskResponse;
+import project.todo.service.todo.task.dto.TaskUpdateRequest;
+import project.todo.service.todo.task.TaskReadService;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RequestMapping("/todos/{todoId}/tasks")
 @RestController
 public class TaskController {
-    private final TaskService taskService;
-
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+    private final TaskReadService taskReadService;
+    private final TaskWriteService taskWriteService;
 
     @GetMapping
     public ResponseEntity<List<TaskResponse>> findTasks(@PathVariable Long todoId) {
-        List<TaskResponse> tasks = taskService.findTasks(todoId);
+        var tasks = taskReadService.findTasks(todoId);
         return ResponseEntity.ok(tasks);
     }
 
     @PostMapping
     public ResponseEntity<Void> add(@PathVariable Long todoId,
                                     @RequestBody TaskAddRequest request) {
-        taskService.add(todoId, request);
+        taskWriteService.add(todoId, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PatchMapping("/{taskId}")
     public ResponseEntity<Void> update(@PathVariable Long taskId,
                                        @RequestBody TaskUpdateRequest request) {
-        taskService.update(taskId, request);
+        taskWriteService.update(taskId, request);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{taskId}/complete")
     public ResponseEntity<Void> completeTask(@PathVariable Long todoId,
                                              @PathVariable Long taskId) {
-        taskService.completeTask(todoId, taskId);
+        taskWriteService.complete(todoId, taskId);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{taskId}")
     public ResponseEntity<Void> delete(@PathVariable Long taskId) {
-        taskService.delete(taskId);
+        taskWriteService.delete(taskId);
         return ResponseEntity.noContent().build();
     }
 }
