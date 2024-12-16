0053415bfa669ac96c2880db18701e09371678e8
youngrok <yr951103@gmail.com>
2024-12-16 14:09:52 +0900

    feat: TodoWithTasksResponse - Todo 상세 조회 응답용 DTO 객체 생성
    - Todo 정보와 Task 간략한 정보 함께 출력

---
+++ src/main/java/project/todo/service/todo/dto/TodoWithTasksResponse.java
@@ -0,0 +1,52 @@
+package project.todo.service.todo.dto;
+
+import project.todo.model.todo.Todo;
+import project.todo.model.todo.TodoStatus;
+import project.todo.model.todo.task.Task;
+import project.todo.model.todo.task.TaskStatus;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record TodoWithTasksResponse(
+        Long todoId,
+        String title,
+        LocalDate deadline,
+        LocalDateTime createdAt,
+        TodoStatus status,
+        LocalDateTime completedAt,
+        List<TaskSimpleResponse> tasks
+) {
+
+    public static TodoWithTasksResponse from(Todo todo, List<Task> tasks) {
+        var taskResponses = tasks.stream()
+                .map(TaskSimpleResponse::from)
+                .toList();
+
+        return new TodoWithTasksResponse(
+                todo.getId(),
+                todo.getTitle(),
+                todo.getDeadline().toLocalDate(),
+                todo.getCreatedAt(),
+                todo.getStatus(),
+                todo.getCompletedAt(),
+                taskResponses
+        );
+    }
+
+    private record TaskSimpleResponse(
+            Long taskId,
+            String content,
+            TaskStatus status
+    ) {
+
+        private static TaskSimpleResponse from(Task task) {
+            return new TaskSimpleResponse(
+                    task.getId(),
+                    task.getContent(),
+                    task.getStatus()
+            );
+        }
+    }
+}
