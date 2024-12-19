90b6a650001f6d4c7f4e71fa0cf67da5e8e28829
youngrok <yr951103@gmail.com>
2024-12-19 15:58:49 +0900

    test: Member - 도메인 테스트 코드 수정
    - 패키지 이동으로 인한 경로 수정
    - 생성 로직 테스트 코드 추가

--- src/test/java/project/todo/model/member/MemberTest.java
+++ src/test/java/project/todo/model/member/MemberTest.java
@@ -1,26 +1,32 @@
-package project.todo.model;
+package project.todo.model.member;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import project.todo.model.member.Member;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class MemberTest {
 
+    @DisplayName("이름을 입력받으면 새로운 Member 객체가 생성된다.")
+    @Test
+    void initMember() {
+
+        assertDoesNotThrow(() -> new Member("사용자"));
+    }
+
     @ValueSource(strings = {" ", "   "})
     @NullAndEmptySource
-    @ParameterizedTest(name = "객체 생성 시 빈 문자열 또는 공백이면 예외 발생")
+    @ParameterizedTest(name = "객체 생성 시 공백이면 예외 발생")
     void initMemberWithEmptyName(String name) {
 
         assertThatThrownBy(() -> new Member(name))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("이름을 입력해주세요.");
     }
-
 
     @DisplayName("객체 생성 시 이름이 10자를 초과하면 예외 발생")
     @Test
