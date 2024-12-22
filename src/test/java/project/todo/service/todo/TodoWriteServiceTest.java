3d4669b5254f40f689c0b185e1663cbbb874992e
youngrok <yr951103@gmail.com>
2024-12-22 19:16:45 +0900

    test: Todo 완료/미완료 서비스 테스트 코드 추가 및 전체 코드 개선
    - 주석 없이 given, when, then 형식에 맞춰 개행으로 구분하여 가독성 향상
    - deadline 재사용하도록 코드 수정

--- src/test/java/project/todo/service/todo/TodoWriteServiceTest.java
+++ src/test/java/project/todo/service/todo/TodoWriteServiceTest.java
@@ -1,6 +1,5 @@
 package project.todo.service.todo;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,14 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import project.todo.model.member.Member;
 import project.todo.model.todo.Todo;
+import project.todo.model.todo.TodoStatus;
+import project.todo.model.todo.task.Task;
 import project.todo.repository.member.MemberRepository;
 import project.todo.repository.todo.TodoRepository;
+import project.todo.repository.todo.task.TaskRepository;
 import project.todo.service.todo.dto.TodoCreateRequest;
 import project.todo.service.todo.dto.TodoUpdateRequest;
+import project.todo.service.todo.task.TaskWriteService;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,12 +30,18 @@ class TodoWriteServiceTest {
     private MemberRepository memberRepository;
 
     @Autowired
+    private TodoRepository todoRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
     private TodoWriteService todoWriteService;
 
     @Autowired
-    private TodoRepository todoRepository;
+    private TaskWriteService taskWriteService;
 
-    private final LocalDateTime afterDeadline = LocalDateTime.of(
+    private final LocalDateTime deadline = LocalDateTime.of(
             2025, 1, 2, 23, 59, 59
     );
 
@@ -42,41 +50,35 @@ class TodoWriteServiceTest {
         var member = new Member("user");
         memberRepository.save(member);
 
-        var todos = List.of(
-                new Todo(
-                        member.getId(),
-                        "todo1",
-                        LocalDate.of(2025, 1, 1)
-                ),
-                new Todo(
-                        member.getId(),
-                        "todo2",
-                        LocalDate.of(2025, 1, 1)
-                ),
-                new Todo(
-                        member.getId(),
-                        "todo3",
-                        LocalDate.of(2025, 1, 1)
-                )
+        var todo = new Todo(
+                member.getId(),
+                "todo",
+                LocalDate.of(2025, 1, 1)
         );
-        todoRepository.saveAll(todos);
+        todoRepository.save(todo);
+
+        var task = new Task(
+                todo,
+                "task"
+        );
+        taskRepository.save(task);
     }
 
     @DisplayName("사용자는 새로운 Todo를 작성할 수 있다.")
     @Test
     void createTodo() {
         var member = memberRepository.findAll().get(0);
-        var deadline = LocalDate.of(2025, 1, 1);
-        var newTodo = new TodoCreateRequest(
+        var request = new TodoCreateRequest(
                 "new Todo",
-                deadline
+                deadline.toLocalDate()
         );
-        todoWriteService.create(member.getId(), newTodo);
+
+        todoWriteService.create(member.getId(), request);
 
         var todos = todoRepository.findAll();
-        assertThat(todos).hasSize(4);
-        assertThat(todos.get(3).getTitle()).isEqualTo("new Todo");
-        assertThat(todos.get(3).getDeadline()).isEqualTo(deadline.atTime(23, 59, 59));
+        assertThat(todos).hasSize(2);
+        assertThat(todos.get(1).getTitle()).isEqualTo("new Todo");
+        assertThat(todos.get(1).getDeadline()).isEqualTo(deadline);
     }
 
     @DisplayName("Todo의 제목과 마감일을 수정할 수 있다.")
@@ -85,14 +87,15 @@ class TodoWriteServiceTest {
         var todo = todoRepository.findAll().get(0);
         var request = new TodoUpdateRequest(
                 "update Todo",
-                LocalDate.of(2025, 1, 2)
+                deadline.toLocalDate()
         );
-        todoWriteService.update(todo.getId(), request);
-        var updatedTodo = todoRepository.findById(todo.getId())
-                .orElseThrow(() -> new EntityNotFoundException("Todo가 없습니다."));
 
+        todoWriteService.update(todo.getId(), request);
+
+        var updatedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("todo not found"));
         assertThat(updatedTodo.getTitle()).isEqualTo("update Todo");
-        assertThat(updatedTodo.getDeadline()).isEqualTo(afterDeadline);
+        assertThat(updatedTodo.getDeadline()).isEqualTo(deadline);
     }
 
     @DisplayName("Todo의 제목만 수정할 수 있다.")
@@ -104,10 +107,11 @@ class TodoWriteServiceTest {
                 "update Todo",
                 null
         );
-        todoWriteService.update(todo.getId(), request);
-        var updatedTodo = todoRepository.findById(todo.getId())
-                .orElseThrow(() -> new EntityNotFoundException("Todo가 없습니다."));
 
+        todoWriteService.update(todo.getId(), request);
+
+        var updatedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
         assertThat(updatedTodo.getTitle()).isEqualTo("update Todo");
         assertThat(updatedTodo.getDeadline()).isEqualTo(beforeDeadline);
     }
@@ -119,23 +123,54 @@ class TodoWriteServiceTest {
         var beforeTitle = todo.getTitle();
         var request = new TodoUpdateRequest(
                 null,
-                LocalDate.of(2025, 1, 2)
+                deadline.toLocalDate()
         );
-        todoWriteService.update(todo.getId(), request);
-        var updatedTodo = todoRepository.findById(todo.getId())
-                .orElseThrow(() -> new EntityNotFoundException("Todo가 없습니다."));
 
+        todoWriteService.update(todo.getId(), request);
+
+        var updatedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
         assertThat(updatedTodo.getTitle()).isEqualTo(beforeTitle);
-        assertThat(updatedTodo.getDeadline()).isEqualTo(afterDeadline);
+        assertThat(updatedTodo.getDeadline()).isEqualTo(deadline);
+    }
+
+    @DisplayName("Task가 전부 완료된 경우 Todo를 완료할 수 있다.")
+    @Test
+    void completeTodo() {
+        var todo = todoRepository.findAll().get(0);
+        var task = taskRepository.findAll().get(0);
+        taskWriteService.complete(todo.getId(), task.getId());
+        todoWriteService.incomplete(todo.getId());
+
+        todoWriteService.complete(todo.getId());
+
+        var completedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
+        assertThat(completedTodo.getStatus()).isEqualTo(TodoStatus.COMPLETED);
+    }
+
+    @DisplayName("Todo를 미완료 처리할 수 있다.")
+    @Test
+    void incompleteTodo() {
+        var todo = todoRepository.findAll().get(0);
+        var task = taskRepository.findAllByTodoId(todo.getId()).get(0);
+        taskWriteService.complete(todo.getId(), task.getId());
+
+        todoWriteService.incomplete(todo.getId());
+
+        var incompletedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
+        assertThat(incompletedTodo.getStatus()).isEqualTo(TodoStatus.INCOMPLETE);
     }
 
     @DisplayName("Todo를 삭제할 수 있다.")
     @Test
     void deleteTodo() {
         var todo = todoRepository.findAll().get(0);
-        todoWriteService.delete(todo.getId());
-        var deletedTodo = todoRepository.findById(todo.getId());
 
+        todoWriteService.delete(todo.getId());
+
+        var deletedTodo = todoRepository.findById(todo.getId());
         assertThat(deletedTodo).isEmpty();
     }
 }
