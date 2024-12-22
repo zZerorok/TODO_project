e37a0bf125e219e9bb1f6196997ac6b189ff36b2
youngrok <yr951103@gmail.com>
2024-12-22 19:25:33 +0900

    test: Task 완료/미완료 예외 상황 테스트 코드 추가 및 가독성 개선
    - 가독성 향상을 위한 코드 리팩토링 및 불필요한 코드 제거
    - 주석 없이 given, when, then 형식에 맞춰 개행으로 구분하여 가독성 향상

--- src/test/java/project/todo/service/todo/task/TaskWriteServiceTest.java
+++ src/test/java/project/todo/service/todo/task/TaskWriteServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import project.todo.model.todo.Todo;
+import project.todo.model.todo.TodoStatus;
 import project.todo.model.todo.task.Task;
 import project.todo.model.todo.task.TaskStatus;
 import project.todo.repository.todo.TodoRepository;
@@ -15,6 +16,7 @@ import project.todo.service.todo.task.dto.TaskAddRequest;
 import project.todo.service.todo.task.dto.TaskUpdateRequest;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,22 +42,26 @@ class TaskWriteServiceTest {
         );
         todoRepository.save(todo);
 
-        var task = new Task(
-                todo,
-                "new Task"
+        var tasks = List.of(
+                new Task(
+                        todo,
+                        "task1"
+                ),
+                new Task(
+                        todo,
+                        "task2"
+                )
         );
-        taskRepository.save(task);
+        taskRepository.saveAll(tasks);
     }
 
     @DisplayName("Task를 추가할 수 있다.")
     @Test
     void addTask() {
         var todo = todoRepository.findAll().get(0);
-        var request1 = new TaskAddRequest("new Task1");
-        var request2 = new TaskAddRequest("new Task2");
+        var request = new TaskAddRequest("new Task");
 
-        taskWriteService.add(todo.getId(), request1);
-        taskWriteService.add(todo.getId(), request2);
+        taskWriteService.add(todo.getId(), request);
 
         var tasks = taskRepository.findAll();
         assertThat(tasks).hasSize(3);
@@ -65,31 +71,55 @@ class TaskWriteServiceTest {
     @Test
     void updateTask() {
         var task = taskRepository.findAll().get(0);
-        assertThat(task.getContent()).isEqualTo("new Task");
+        var request = new TaskUpdateRequest("update Task");
 
-        taskWriteService.update(
-                task.getId(),
-                new TaskUpdateRequest("update Task")
-        );
+        taskWriteService.update(task.getId(), request);
+
         var updatedTask = taskRepository.findById(task.getId())
                 .orElseThrow(() -> new RuntimeException("Task not found"));
-
         assertThat(updatedTask.getId()).isEqualTo(task.getId());
-        assertThat(updatedTask.getContent()).isEqualTo("update Task");
+        assertThat(updatedTask.getContent()).isEqualTo(request.content());
     }
 
-    @DisplayName("Task를 완료 처리할 수 있다.")
+    @DisplayName("Task 하나를 완료 처리할 수 있다.")
     @Test
     void completeTask() {
         var todo = todoRepository.findAll().get(0);
         var task = taskRepository.findAll().get(0);
-        assertThat(task.getStatus()).isEqualTo(TaskStatus.INCOMPLETE);
 
         taskWriteService.complete(todo.getId(), task.getId());
+
         var completedTask = taskRepository.findById(task.getId())
                 .orElseThrow(() -> new RuntimeException("Task not found"));
-
         assertThat(completedTask.getStatus()).isEqualTo(TaskStatus.COMPLETED);
+    }
+
+    @DisplayName("Task가 전부 완료되면 Todo도 완료 처리된다.")
+    @Test
+    void completeAllTasks() {
+        var todo = todoRepository.findAll().get(0);
+        var tasks = taskRepository.findAll();
+
+        for (Task task : tasks) {
+            taskWriteService.complete(todo.getId(), task.getId());
+        }
+
+        var completedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
+        assertThat(completedTodo.getStatus()).isEqualTo(TodoStatus.COMPLETED);
+    }
+
+    @DisplayName("모든 Task가 완료 되지 않으면 Todo도 완료 처리 되지 않는다.")
+    @Test
+    void todoWithNotAllTasksComplete() {
+        var todo = todoRepository.findAll().get(0);
+        var task = taskRepository.findAll().get(0);
+
+        taskWriteService.complete(todo.getId(), task.getId());
+
+        var completedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
+        assertThat(completedTodo.getStatus()).isEqualTo(TodoStatus.INCOMPLETE);
     }
 
     @DisplayName("Task를 미완료 처리할 수 있다.")
@@ -98,23 +128,41 @@ class TaskWriteServiceTest {
         var todo = todoRepository.findAll().get(0);
         var task = taskRepository.findAll().get(0);
         taskWriteService.complete(todo.getId(), task.getId());
+
         var completedTask = taskRepository.findById(task.getId())
                 .orElseThrow(() -> new RuntimeException("Task not found"));
-        assertThat(completedTask.getStatus()).isEqualTo(TaskStatus.COMPLETED);
-
         taskWriteService.incomplete(todo.getId(), completedTask.getId());
+
         var incompletedTask = taskRepository.findById(completedTask.getId())
                 .orElseThrow(() -> new RuntimeException("Task not found"));
         assertThat(incompletedTask.getStatus()).isEqualTo(TaskStatus.INCOMPLETE);
+    }
+
+    @DisplayName("Task 하나라도 미완료 처리하면 Todo도 미완료 처리된다.")
+    @Test
+    void todoWithOnceIncomplete() {
+        var todo = todoRepository.findAll().get(0);
+        var tasks = taskRepository.findAll();
+        for (Task task : tasks) {
+            taskWriteService.complete(todo.getId(), task.getId());
+        }
+
+        var completedTask = taskRepository.findAll().get(0);
+        taskWriteService.incomplete(todo.getId(), completedTask.getId());
+
+        var incompletedTodo = todoRepository.findById(todo.getId())
+                .orElseThrow(() -> new RuntimeException("Todo not found"));
+        assertThat(incompletedTodo.getStatus()).isEqualTo(TodoStatus.INCOMPLETE);
     }
 
     @DisplayName("Task를 삭제할 수 있다.")
     @Test
     void deleteTask() {
         var task = taskRepository.findAll().get(0);
-        taskWriteService.delete(task.getId());
-        var deletedTask = taskRepository.findById(task.getId());
 
+        taskWriteService.delete(task.getId());
+
+        var deletedTask = taskRepository.findById(task.getId());
         assertThat(deletedTask).isEmpty();
     }
 }
