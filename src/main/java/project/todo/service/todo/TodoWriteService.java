c55d043c2ab09fa87760623c9b6a79c02765338e
youngrok <yr951103@gmail.com>
2024-12-22 18:18:09 +0900

    feat: TodoWriteService - Todo 완료/미완료 처리 기능 구현

--- src/main/java/project/todo/service/todo/TodoWriteService.java
+++ src/main/java/project/todo/service/todo/TodoWriteService.java
@@ -6,10 +6,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.todo.model.member.Member;
 import project.todo.model.todo.Todo;
+import project.todo.model.todo.task.Task;
 import project.todo.repository.member.MemberRepository;
 import project.todo.repository.todo.TodoRepository;
+import project.todo.repository.todo.task.TaskRepository;
 import project.todo.service.todo.dto.TodoCreateRequest;
 import project.todo.service.todo.dto.TodoUpdateRequest;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Transactional
@@ -17,6 +21,7 @@ import project.todo.service.todo.dto.TodoUpdateRequest;
 public class TodoWriteService {
     private final MemberRepository memberRepository;
     private final TodoRepository todoRepository;
+    private final TaskRepository taskRepository;
 
     public void create(Long memberId, TodoCreateRequest request) {
         var member = getMember(memberId);
@@ -37,6 +42,20 @@ public class TodoWriteService {
         );
     }
 
+    public void complete(Long todoId) {
+        var tasks = getTasks(todoId);
+
+        if (isAllTasksCompleted(tasks)) {
+            var todo = getTodo(todoId);
+            todo.complete();
+        }
+    }
+
+    public void incomplete(Long todoId) {
+        var todo = getTodo(todoId);
+        todo.incomplete();
+    }
+
     public void delete(Long todoId) {
         var todo = getTodo(todoId);
 
@@ -51,5 +70,20 @@ public class TodoWriteService {
     private Todo getTodo(long todoId) {
         return todoRepository.findById(todoId)
                 .orElseThrow(() -> new EntityNotFoundException("해당 Todo가 존재하지 않습니다."));
+    }
+
+    private List<Task> getTasks(long todoId) {
+        var tasks = taskRepository.findAllByTodoId(todoId);
+
+        if (tasks.isEmpty()) {
+            throw new IllegalStateException("Task를 생성해주세요.");
+        }
+
+        return tasks;
+    }
+
+    private boolean isAllTasksCompleted(List<Task> tasks) {
+        return tasks.stream()
+                .allMatch(it -> it.getStatus().isCompleted());
     }
 }
