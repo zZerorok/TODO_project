8300675131db2d7c45c7a310eb3f00464c7fdc12
youngrok <yr951103@gmail.com>
2025-01-02 21:55:19 +0900

    feat: Member - 로그인 기능 구현을 위한 필드 (loginId, password, email) 추가

--- src/main/java/project/todo/model/member/Member.java
+++ src/main/java/project/todo/model/member/Member.java
@@ -1,9 +1,14 @@
 package project.todo.model.member;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -15,11 +20,23 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String loginId;
+    private String password;
+    private String email;
+    private LocalDateTime createdAt;
 
-    public Member(String name) {
+    public Member(String name, String loginId, String password, String email) {
+        this(name, loginId, password, email, LocalDateTime.now());
+    }
+
+    public Member(String name, String loginId, String password, String email, LocalDateTime createdAt) {
         validateName(name);
 
         this.name = name;
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
     }
 
     private void validateName(String name) {
