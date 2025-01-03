a2bc5b23bd256d73d7b6ef8d23f5272f1796bc17
youngrok <yr951103@gmail.com>
2025-01-03 14:34:07 +0900

    feat: MemberController - 회원가입 폼 처리 및 유효성 검사 추가
    - 어노테이션 사용하여 생성자 주입 처리
    - 회원가입 폼 초기값 설정
    - 유효성 검증 실패 시 에러 메세지 처리 및 화면 표시

--- src/main/java/project/todo/controller/MemberController.java
+++ src/main/java/project/todo/controller/MemberController.java
@@ -1,20 +1,21 @@
 package project.todo.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import project.todo.model.member.Member;
 import project.todo.service.member.MemberCreateRequest;
 import project.todo.service.member.MemberService;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/members")
 @Controller
 public class MemberController {
     private final MemberService memberService;
-
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
 
     @GetMapping("/hello")
     public String hello(@RequestParam String name, Model model) {
@@ -22,15 +23,27 @@ public class MemberController {
         return "hello";
     }
 
-    @PostMapping("/save")
-    public String save(@ModelAttribute MemberCreateRequest request) {
-        Member savedMember = memberService.save(request);
-        return "redirect:/member/hello?name=" + savedMember.getName();
+    @GetMapping("/register-form")
+    public String registerForm(Model model) {
+        var registerForm = new MemberCreateRequest(
+                null,
+                null,
+                null,
+                null
+        );
+        model.addAttribute("form", registerForm);
+        return "members/register-form";
     }
 
-    @GetMapping("/register-form")
-    public String registerForm() {
-        return "members/register-form";
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("form") MemberCreateRequest request,
+                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "members/register-form";
+        }
+
+        memberService.register(request);
+        return "redirect:/";
     }
 
     @GetMapping("/login-form")
