fecab6d3128524206b787c9d0c29656ba961b606
youngrok <yr951103@gmail.com>
2025-01-03 14:24:34 +0900

    refactor: MemberService - 회원 가입 로직 수정
    - 어노테이션 사용하여 생성자 주입 처리
    - DTO 필드 변경에 의한 전달인자 변경

--- src/main/java/project/todo/service/member/MemberService.java
+++ src/main/java/project/todo/service/member/MemberService.java
@@ -1,20 +1,23 @@
 package project.todo.service.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.todo.model.member.Member;
 import project.todo.repository.member.MemberRepository;
 
+@RequiredArgsConstructor
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    public void register(MemberCreateRequest request) {
+        var member = new Member(
+                request.name(),
+                request.loginId(),
+                request.password(),
+                request.email()
+        );
 
-    public Member save(MemberCreateRequest request) {
-        Member member = new Member(request.name());
         memberRepository.save(member);
-        return member;
     }
 }
