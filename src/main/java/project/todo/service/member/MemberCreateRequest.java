ff8a30c527bd9bacc07a5fa20b09daf9573fdf8b
youngrok <yr951103@gmail.com>
2025-01-03 14:21:16 +0900

    feat: MemberCreateRequest - 회원 가입을 위한 필드 추가 및 공백 검증 추가

--- src/main/java/project/todo/service/member/MemberCreateRequest.java
+++ src/main/java/project/todo/service/member/MemberCreateRequest.java
@@ -1,6 +1,20 @@
 package project.todo.service.member;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
 public record MemberCreateRequest(
-        String name
+        @NotEmpty(message = "이름은 공백일 수 없습니다.")
+        String name,
+
+        @NotEmpty(message = "아이디는 공백일 수 없습니다.")
+        String loginId,
+
+        @NotEmpty(message = "비밀번호는 공백일 수 없습니다.")
+        String password,
+
+        @NotEmpty(message = "이메일은 공백일 수 없습니다.")
+        @Email(message = "이메일 형식이 올바르지 않습니다.")
+        String email
 ) {
 }
