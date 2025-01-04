90731b8b97be22ba8b5d4bca07a4fe21a0c1d68b
youngrok <yr951103@gmail.com>
2025-01-04 19:34:32 +0900

    feat: MemberController - 로그인 폼 및 로그인 처리 로직 추가

--- src/main/java/project/todo/controller/MemberController.java
+++ src/main/java/project/todo/controller/MemberController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import project.todo.model.member.Member;
 import project.todo.service.member.MemberCreateRequest;
+import project.todo.service.member.MemberLoginRequest;
 import project.todo.service.member.MemberService;
 
 @Slf4j
@@ -18,8 +20,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/hello")
-    public String hello(@RequestParam String name, Model model) {
-        model.addAttribute("name", name);
+    public String hello(@ModelAttribute Member loginMember,
+                        Model model) {
+        model.addAttribute("loginMember", loginMember);
         return "hello";
     }
 
@@ -47,7 +50,31 @@ public class MemberController {
     }
 
     @GetMapping("/login-form")
-    public String loginForm() {
+    public String loginForm(Model model) {
+        var loginForm = new MemberLoginRequest(
+                null,
+                null
+        );
+        model.addAttribute("loginForm", loginForm);
         return "members/login-form";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("loginForm") MemberLoginRequest request,
+                        BindingResult bindingResult,
+                        Model model) {
+        if (bindingResult.hasErrors()) {
+            return "members/login-form";
+        }
+
+        var loginMember = memberService.login(request);
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "members/login-form";
+        }
+
+        model.addAttribute("loginMember", loginMember);
+        return "hello";
     }
 }
