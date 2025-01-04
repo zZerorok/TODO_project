0adab9e6bde98ade3a94ba282fbcb02d05e3de45
youngrok <yr951103@gmail.com>
2025-01-04 19:29:04 +0900

    feat: MemberRepository - 로그인 ID로 회원 조회 메서드 추가

--- src/main/java/project/todo/repository/member/MemberRepository.java
+++ src/main/java/project/todo/repository/member/MemberRepository.java
@@ -3,5 +3,9 @@ package project.todo.repository.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.todo.model.member.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
 }
