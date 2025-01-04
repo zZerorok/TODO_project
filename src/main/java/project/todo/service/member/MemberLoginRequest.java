91c18ad8de16f96abd5f401b9c4db483d5f6aefd
youngrok <yr951103@gmail.com>
2025-01-04 19:32:16 +0900

    feat: MemberLoginRequest - 로그인 요청 데이터를 처리할 DTO 추가

---
+++ src/main/java/project/todo/service/member/MemberLoginRequest.java
@@ -0,0 +1,12 @@
+package project.todo.service.member;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public record MemberLoginRequest(
+        @NotEmpty(message = "아이디를 입력해주세요.")
+        String loginId,
+
+        @NotEmpty(message = "비밀번호를 입력해주세요.")
+        String password
+) {
+}
