5022c40898769c5106ed14d8f58e0ed7c66b8916
youngrok <yr951103@gmail.com>
2025-01-05 21:46:39 +0900

    feat: MemberRepository - loginId 존재 여부 확인 메서드 추가

--- src/main/java/project/todo/repository/member/MemberRepository.java
+++ src/main/java/project/todo/repository/member/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByLoginId(String loginId);
+
+    boolean existsByLoginId(String loginId);
 }
