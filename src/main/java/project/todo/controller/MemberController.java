0cfab71bc5be43fe2d2e7eb8a793abd077c6c328
youngrok <yr951103@gmail.com>
2025-01-05 20:10:52 +0900

    feat: MemberController - 로그인 시 세션에 Member 객체 추가

--- src/main/java/project/todo/controller/MemberController.java
+++ src/main/java/project/todo/controller/MemberController.java
@@ -1,12 +1,17 @@
 package project.todo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import project.todo.model.member.Member;
 import project.todo.service.member.MemberCreateRequest;
 import project.todo.service.member.MemberLoginRequest;
@@ -20,8 +25,19 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/hello")
-    public String hello(@ModelAttribute Member loginMember,
-                        Model model) {
+    public String hello(HttpServletRequest httpRequest,
+                        Model model
+    ) {
+        HttpSession session = httpRequest.getSession(false);
+        if (session == null) {
+            return "redirect:members/login-form";
+        }
+
+        var loginMember = (Member) session.getAttribute("loginMember");
+        if (loginMember == null) {
+            return "redirect:members/login-form";
+        }
+
         model.addAttribute("loginMember", loginMember);
         return "hello";
     }
@@ -40,7 +56,8 @@ public class MemberController {
 
     @PostMapping("/register")
     public String register(@Valid @ModelAttribute("form") MemberCreateRequest request,
-                           BindingResult bindingResult) {
+                           BindingResult bindingResult
+    ) {
         if (bindingResult.hasErrors()) {
             return "members/register-form";
         }
@@ -60,14 +77,18 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("loginForm") MemberLoginRequest request,
+    public String login(@Valid @ModelAttribute("loginForm") MemberLoginRequest loginRequest,
                         BindingResult bindingResult,
-                        Model model) {
+                        HttpServletRequest httpRequest,
+                        Model model
+    ) {
         if (bindingResult.hasErrors()) {
             return "members/login-form";
         }
 
-        var loginMember = memberService.login(request);
+        var loginMember = memberService.login(loginRequest);
+        HttpSession session = httpRequest.getSession();
+        session.setAttribute("loginMember", loginMember);
 
         if (loginMember == null) {
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
