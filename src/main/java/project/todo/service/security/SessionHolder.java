4dc832589601d64b56e599dc280ad9fce6550294
youngrok <yr951103@gmail.com>
2025-01-07 21:23:17 +0900

    feat: SessionHolder - 사용자 세션 관리 인터페이스 구현

---
+++ src/main/java/project/todo/service/security/SessionHolder.java
@@ -0,0 +1,22 @@
+package project.todo.service.security;
+
+/**
+ * 사용자 세션을 관리하는 컴포넌트
+ */
+public interface SessionHolder {
+
+    /**
+     * @return 로그인된 멤버 객체
+     */
+    LoginMember getSession();
+
+    /**
+     * @param loginMember 로그인 시킬 멤버 객체
+     */
+    void setSession(LoginMember loginMember);
+
+    /**
+     * 세션 삭제
+     */
+    void removeSession();
+}
