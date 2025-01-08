be773a14415ca7f2ac2001f30be7224adeceb3b3
youngrok <yr951103@gmail.com>
2025-01-08 14:46:35 +0900

    feat: LoginService - 로그인 시 비밀번호 해시 처리 기능 구현

--- src/main/java/project/todo/service/member/LoginService.java
+++ src/main/java/project/todo/service/member/LoginService.java
@@ -2,18 +2,22 @@ package project.todo.service.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import project.todo.service.security.LoginMember;
 import project.todo.repository.member.MemberRepository;
+import project.todo.service.security.LoginMember;
+import project.todo.service.security.PasswordEncrypt;
 
 @RequiredArgsConstructor
 @Service
 public class LoginService {
     private final MemberRepository memberRepository;
+    private final PasswordEncrypt passwordEncrypt;
 
     public LoginMember login(MemberLoginRequest request) {
         var member = memberRepository.findByLoginId(request.loginId())
                 .orElseThrow(() -> new IllegalStateException("로그인 정보가 일치하지 않습니다."));
-        member.validatePassword(request.password());
+
+        var hashedPassword = passwordEncrypt.toHash(request.password());
+        member.validatePassword(hashedPassword);
 
         return LoginMember.from(member);
     }
