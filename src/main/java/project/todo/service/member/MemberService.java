51e6ebe58317a08be343d281125776666c3203ad
youngrok <yr951103@gmail.com>
2025-01-08 14:46:17 +0900

    feat: MemberService - 회원가입 시 비밀번호 해시 처리 기능 구현

--- src/main/java/project/todo/service/member/MemberService.java
+++ src/main/java/project/todo/service/member/MemberService.java
@@ -4,21 +4,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.todo.model.member.Member;
 import project.todo.repository.member.MemberRepository;
+import project.todo.service.security.PasswordEncrypt;
 
 @RequiredArgsConstructor
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncrypt passwordEncrypt;
 
     public void register(MemberCreateRequest request) {
         if (memberRepository.existsByLoginId(request.loginId())) {
             throw new IllegalStateException("이미 존재하는 아이디 입니다.");
         }
 
+        var hashedPassword = passwordEncrypt.toHash(request.password());
         var member = new Member(
                 request.name(),
                 request.loginId(),
-                request.password(),
+                hashedPassword,
                 request.email()
         );
 
