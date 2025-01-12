d37f4bec7bda91b878761e98035093df5dc3487f
youngrok <yr951103@gmail.com>
2025-01-12 23:01:22 +0900

    feat: TodoRepository - 특정 회원과 상태에 맞는 Todo 목록 조회 메서드 추가

--- src/main/java/project/todo/repository/todo/TodoRepository.java
+++ src/main/java/project/todo/repository/todo/TodoRepository.java
@@ -2,10 +2,13 @@ package project.todo.repository.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.todo.model.todo.Todo;
+import project.todo.model.todo.TodoStatus;
 
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     List<Todo> findAllByMemberId(Long memberId);
+
+    List<Todo> findByMemberIdAndStatus(Long memberId, TodoStatus status);
 }
