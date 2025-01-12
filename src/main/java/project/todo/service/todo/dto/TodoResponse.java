0dc33b6dc1da9f783c1aab7be81e7a89bb7091e5
youngrok <yr951103@gmail.com>
2025-01-12 22:59:54 +0900

    feat: TodoResponse 통합
    - TodoSimpleResponse 삭제
    - TodoDetailResponse -> TodoResponse 이름 변경

--- src/main/java/project/todo/service/todo/dto/TodoResponse.java
+++ src/main/java/project/todo/service/todo/dto/TodoResponse.java
@@ -4,19 +4,22 @@ import project.todo.model.todo.Todo;
 import project.todo.model.todo.TodoStatus;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
-public record TodoSimpleResponse(
+public record TodoResponse(
         Long id,
         String title,
         LocalDate deadline,
+        LocalDateTime createdAt,
         TodoStatus status
 ) {
 
-    public static TodoSimpleResponse from(Todo todo) {
-        return new TodoSimpleResponse(
+    public static TodoResponse from(Todo todo) {
+        return new TodoResponse(
                 todo.getId(),
                 todo.getTitle(),
                 todo.getDeadline().toLocalDate(),
+                todo.getCreatedAt(),
                 todo.getStatus()
         );
     }
