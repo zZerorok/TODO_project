875d53bb1894ce243704fb03ba027afd21d22a82
youngrok <yr951103@gmail.com>
2025-01-13 23:18:11 +0900

    refactor: TodoWithTasksResponse - TaskSimpleResponse 제거하고 TaskDetailResponse 재사용

--- src/main/java/project/todo/service/todo/dto/TodoWithTasksResponse.java
+++ src/main/java/project/todo/service/todo/dto/TodoWithTasksResponse.java
@@ -3,7 +3,7 @@ package project.todo.service.todo.dto;
 import project.todo.model.todo.Todo;
 import project.todo.model.todo.TodoStatus;
 import project.todo.model.todo.task.Task;
-import project.todo.model.todo.task.TaskStatus;
+import project.todo.service.todo.task.dto.TaskDetailResponse;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,12 +15,12 @@ public record TodoWithTasksResponse(
         LocalDate deadline,
         LocalDateTime createdAt,
         TodoStatus status,
-        List<TaskSimpleResponse> tasks
+        List<TaskDetailResponse> tasks
 ) {
 
     public static TodoWithTasksResponse from(Todo todo, List<Task> tasks) {
         var taskResponses = tasks.stream()
-                .map(TaskSimpleResponse::from)
+                .map(TaskDetailResponse::from)
                 .toList();
 
         return new TodoWithTasksResponse(
@@ -31,20 +31,5 @@ public record TodoWithTasksResponse(
                 todo.getStatus(),
                 taskResponses
         );
-    }
-
-    private record TaskSimpleResponse(
-            Long taskId,
-            String content,
-            TaskStatus status
-    ) {
-
-        private static TaskSimpleResponse from(Task task) {
-            return new TaskSimpleResponse(
-                    task.getId(),
-                    task.getContent(),
-                    task.getStatus()
-            );
-        }
     }
 }
