8e14d696f2bb71b951bf8ded955a88eb34ded130
youngrok <yr951103@gmail.com>
2025-01-16 01:36:16 +0900

    refactor: TodoRepository - 복잡한 쿼리 조건을 애플리케이션 로직으로 대체하여 유지보수 용이성 증가

--- src/main/java/project/todo/repository/todo/TodoRepository.java
+++ src/main/java/project/todo/repository/todo/TodoRepository.java
@@ -2,16 +2,10 @@ package project.todo.repository.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.todo.model.todo.Todo;
-import project.todo.model.todo.TodoStatus;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     List<Todo> findAllByMemberId(Long memberId);
-
-    List<Todo> findByMemberIdAndStatus(Long memberId, TodoStatus status);
-
-    Optional<Todo> findByIdAndMemberId(Long todoId, Long memberId);
 }
