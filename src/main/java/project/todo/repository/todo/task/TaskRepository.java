ea31c52414d796f1b2112552d81729778174888b
youngrok <yr951103@gmail.com>
2025-01-18 09:15:32 +0900

    feat: TaskRepository - Todo와 상태 기반 존재 여부 확인 메서드 추가

--- src/main/java/project/todo/repository/todo/task/TaskRepository.java
+++ src/main/java/project/todo/repository/todo/task/TaskRepository.java
@@ -1,6 +1,8 @@
 package project.todo.repository.todo.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.todo.model.todo.Status;
+import project.todo.model.todo.Todo;
 import project.todo.model.todo.task.Task;
 
 import java.util.List;
@@ -10,4 +12,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByTodoId(Long todoId);
 
     void deleteAllByTodoId(Long todoId);
+
+    Boolean existsByTodoAndStatus(Todo todo, Status status);
 }
