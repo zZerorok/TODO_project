41f5fce8b66535ea65d7a92f61fe3778e60b8f42
youngrok <yr951103@gmail.com>
2025-01-25 02:25:48 +0900

    feat: LoginFailedException - 로그인 실패 시 발생하는 커스텀 예외 클래스 추가

---
+++ src/main/java/project/todo/exception/member/LoginFailedException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.member;
+
+public class LoginFailedException extends MemberException {
+
+    public LoginFailedException(String message) {
+        super(message);
+    }
+}
