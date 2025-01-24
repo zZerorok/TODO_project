9d255c4ef938625486d67beb8df10df176259e6a
youngrok <yr951103@gmail.com>
2025-01-24 14:27:27 +0900

    feat: TaskNotInTodoExceptio - Task가 Todo에 포함되어 있지 않을 경우 발생하는 커스텀 예외
    - 클래스명 간소화
    - RuntimeException 대신 TaskException 상속
    - TaskNotContainsInTodoException 클래스 삭제

--- src/main/java/project/todo/exception/todo/task/TaskNotContainsInTodoException.java
+++
@@ -1,8 +0,0 @@
-package project.todo.exception.todo.task;
-
-public class TaskNotContainsInTodoException extends RuntimeException {
-
-    public TaskNotContainsInTodoException(Long todoId) {
-        super("해당 Task는 " + todoId + "에 포함되어 있지 않습니다.");
-    }
-}
---
+++ src/main/java/project/todo/exception/todo/task/TaskNotInTodoException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.todo.task;
+
+public class TaskNotInTodoException extends TaskException {
+
+    public TaskNotInTodoException(String message) {
+        super(message);
+    }
+}
