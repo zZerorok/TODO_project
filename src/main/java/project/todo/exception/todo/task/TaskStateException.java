7d3579429b5e1ccc4821656a955231fc59a15e8a
youngrok <yr951103@gmail.com>
2025-01-24 14:45:31 +0900

    feat: TaskStateException - Task 상태 처리 시 발생하는 커스텀 예외 클래스 추가

---
+++ src/main/java/project/todo/exception/todo/task/TaskStateException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.todo.task;
+
+public class TaskStateException extends TaskException {
+
+    public TaskStateException(String message) {
+        super(message);
+    }
+}
