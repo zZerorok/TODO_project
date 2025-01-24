b0d4ff1d4f102ab302f1d8d6d7787d9872058020
youngrok <yr951103@gmail.com>
2025-01-24 13:55:35 +0900

    feat: DeadlineException - 마감일 초과 시 발생하는 커스텀 예외 클래스 추가

---
+++ src/main/java/project/todo/exception/todo/DeadlineException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.todo;
+
+public class DeadlineException extends RuntimeException {
+
+    public DeadlineException(String message) {
+        super(message);
+    }
+}
