43f7a863f5ab2500a45e68aaf2086fef334856cb
youngrok <yr951103@gmail.com>
2025-01-25 02:24:10 +0900

    feat: NameLengthException - Member 이름 길이 초과 시 발생하는 커스텀 예외 클래스 추가

---
+++ src/main/java/project/todo/exception/member/NameLengthException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.member;
+
+public class NameLengthException extends MemberException {
+
+    public NameLengthException(String message) {
+        super(message);
+    }
+}
