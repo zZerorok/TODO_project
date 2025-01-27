ab132d33a4707c50c55d43aef9642b846b7cc621
youngrok <yr951103@gmail.com>
2025-01-28 01:13:31 +0900

    feat: Task 조회 실패 시 발생하는 커스텀 예외 클래스

---
+++ src/main/java/project/todo/exception/todo/task/TaskNotFoundException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.todo.task;
+
+public class TaskNotFoundException extends TaskException {
+
+    public TaskNotFoundException(String message) {
+        super(message);
+    }
+}
