017cc266107b20c5e6a50c4eea0ffebb08f9d4e8
youngrok <yr951103@gmail.com>
2025-01-28 01:13:39 +0900

    feat: Todo 조회 실패 시 발생하는 커스텀 예외 클래스

---
+++ src/main/java/project/todo/exception/todo/TodoNotFoundException.java
@@ -0,0 +1,8 @@
+package project.todo.exception.todo;
+
+public class TodoNotFoundException extends TodoException {
+
+    public TodoNotFoundException(String Message) {
+        super(Message);
+    }
+}
