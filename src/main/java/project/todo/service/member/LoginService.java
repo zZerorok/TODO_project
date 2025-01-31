47ed965d0c8c9fa69b29e82d3bab50610f0b894d
youngrok <yr951103@gmail.com>
2025-01-31 13:30:33 +0900

    refactor: LoginService - 커스텀 예외 처리 및 코드 개선
    - LoginFailedException 커스텀 예외 클래스 적용
    - 메서드 분리로 가독성 향상

--- src/main/java/project/todo/service/member/LoginService.java
+++ src/main/java/project/todo/service/member/LoginService.java
@@ -2,6 +2,8 @@ package project.todo.service.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.todo.exception.member.LoginFailedException;
+import project.todo.model.member.Member;
 import project.todo.repository.member.MemberRepository;
 import project.todo.service.security.LoginMember;
 import project.todo.service.security.PasswordEncrypt;
@@ -13,12 +15,19 @@ public class LoginService {
     private final PasswordEncrypt passwordEncrypt;
 
     public LoginMember login(MemberLoginRequest request) {
-        var member = memberRepository.findByLoginId(request.loginId())
-                .orElseThrow(() -> new IllegalStateException("로그인 정보가 일치하지 않습니다."));
-
-        var hashedPassword = passwordEncrypt.toHash(request.password());
-        member.validatePassword(hashedPassword);
+        var member = getMemberByLoginId(request.loginId());
+        checkPasswordMatches(member, request.password());
 
         return LoginMember.from(member);
+    }
+
+    private Member getMemberByLoginId(String loginId) {
+        return memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new LoginFailedException("로그인 정보가 일치하지 않습니다."));
+    }
+
+    private void checkPasswordMatches(Member member, String password) {
+        var hashedPassword = passwordEncrypt.toHash(password);
+        member.validatePassword(hashedPassword);
     }
 }
