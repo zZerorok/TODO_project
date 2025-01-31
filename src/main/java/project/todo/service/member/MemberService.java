3b230975618ebc3f0fc8a94359385fa34908db93
youngrok <yr951103@gmail.com>
2025-02-01 00:48:06 +0900

    docs: MemberService - 클래스 및 메서드 JavaDoc 주석 추가

--- src/main/java/project/todo/service/member/MemberService.java
+++ src/main/java/project/todo/service/member/MemberService.java
@@ -6,12 +6,19 @@ import project.todo.model.member.Member;
 import project.todo.repository.member.MemberRepository;
 import project.todo.service.security.PasswordEncrypt;
 
+/**
+ * 회원 관련 요청을 처리하는 서비스 클래스
+ */
 @RequiredArgsConstructor
 @Service
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncrypt passwordEncrypt;
 
+    /**
+     * 아이디 중복검사를 거쳐 회원가입을 진행합니다.
+     * @param request 회원가입 요청 객체
+     */
     public void register(MemberCreateRequest request) {
         checkDuplicateLoginId(request.loginId());
         var member = new Member(
