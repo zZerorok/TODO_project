58442d60215e4a863430b847da061750cb1481c8
youngrok <yr951103@gmail.com>
2025-02-03 18:28:38 +0900

    docs: TodoWriteService - 클래스 및 메서드 JavaDoc 주석 추가

--- src/main/java/project/todo/service/todo/TodoWriteService.java
+++ src/main/java/project/todo/service/todo/TodoWriteService.java
@@ -14,6 +14,9 @@ import project.todo.service.security.SessionHolder;
 import project.todo.service.todo.dto.TodoCreateRequest;
 import project.todo.service.todo.dto.TodoUpdateRequest;
 
+/**
+ * Todo 쓰기 작업 요청을 처리하는 서비스 클래스
+ */
 @RequiredArgsConstructor
 @Transactional
 @Service
@@ -22,6 +25,11 @@ public class TodoWriteService {
     private final TaskRepository taskRepository;
     private final SessionHolder sessionHolder;
 
+    /**
+     * 새로운 Todo를 생성합니다.
+     *
+     * @param request Todo 생성 요청 객체
+     */
     public void create(TodoCreateRequest request) {
         var loginMember = getLoginMember();
         var todo = new Todo(
@@ -33,14 +41,28 @@ public class TodoWriteService {
         todoRepository.save(todo);
     }
 
+    /**
+     * 특정 Todo를 수정합니다.
+     *
+     * @param todoId 수정할 Todo의 ID
+     * @param request Todo 수정 요청 객체
+     */
     public void update(Long todoId, TodoUpdateRequest request) {
         var todo = getTodoWithValidation(todoId);
 
         todo.update(request.title(), request.deadline());
     }
 
+    /**
+     * 요청 상태에 따라 Todo의 완료/미완료 처리를 진행합니다.<p>
+     *
+     * - Todo를 완료 처리하려면, 해당 Todo에 포함된 모든 Task가 완료 상태여야 합니다.<p>
+     * - Todo를 미완료 처리하려면, Todo가 완료 상태여야 하며 확인 후 상태를 변경합니다.<p>
+     *
+     * @param todoId
+     * @param status 변경할 요청의 상태(완료 또는 미완료)
+     */
     public void updateStatus(Long todoId, Status status) {
-
         if (status == Status.COMPLETE) {
             complete(todoId);
         }
@@ -50,20 +72,11 @@ public class TodoWriteService {
         }
     }
 
-    private void complete(Long todoId) {
-        var todo = getTodoWithValidation(todoId);
-
-        if (isAllTasksCompleted(todo)) {
-            todo.complete();
-        }
-    }
-
-    private void incomplete(Long todoId) {
-        var todo = getTodoWithValidation(todoId);
-
-        todo.incomplete();
-    }
-
+    /**
+     * 특정 Todo를 삭제합니다.
+     *
+     * @param todoId 삭제할 Todo의 ID
+     */
     public void delete(Long todoId) {
         var todo = getTodoWithValidation(todoId);
 
@@ -91,6 +104,20 @@ public class TodoWriteService {
     private Todo getTodo(long todoId) {
         return todoRepository.findById(todoId)
                 .orElseThrow(() -> new TodoNotFoundException("해당 Todo가 존재하지 않습니다."));
+    }
+
+    private void complete(Long todoId) {
+        var todo = getTodoWithValidation(todoId);
+
+        if (isAllTasksCompleted(todo)) {
+            todo.complete();
+        }
+    }
+
+    private void incomplete(Long todoId) {
+        var todo = getTodoWithValidation(todoId);
+
+        todo.incomplete();
     }
 
     private boolean isAllTasksCompleted(Todo todo) {
