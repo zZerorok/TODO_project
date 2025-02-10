6555f6a02f82a3f423825a5488954e70f34d5fa1
youngrok <yr951103@gmail.com>
2025-02-10 14:34:39 +0900

    feat: TaskController - @Login 어노테이션으로 로그인 사용자 정보 주입

--- src/main/java/project/todo/controller/TaskController.java
+++ src/main/java/project/todo/controller/TaskController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.todo.model.todo.Status;
+import project.todo.service.member.Login;
+import project.todo.service.security.dto.LoginMember;
 import project.todo.service.todo.task.TaskWriteService;
 import project.todo.service.todo.task.dto.TaskAddRequest;
 import project.todo.service.todo.task.dto.TaskUpdateRequest;
@@ -17,39 +19,43 @@ public class TaskController {
 
     @PostMapping
     public ResponseEntity<Void> add(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId,
             @RequestBody TaskAddRequest request
     ) {
-        taskWriteService.add(todoId, request);
+        taskWriteService.add(loginMember, todoId, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PatchMapping("/{taskId}")
     public ResponseEntity<Void> update(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId,
             @PathVariable Long taskId,
             @RequestBody TaskUpdateRequest request
     ) {
-        taskWriteService.update(todoId, taskId, request);
+        taskWriteService.update(loginMember, todoId, taskId, request);
         return ResponseEntity.noContent().build();
     }
 
     @PatchMapping("/{taskId}/status")
     public ResponseEntity<Void> updateStatus(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId,
             @PathVariable Long taskId,
             @RequestParam Status status
     ) {
-        taskWriteService.updateStatus(todoId, taskId, status);
+        taskWriteService.updateStatus(loginMember, todoId, taskId, status);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{taskId}")
     public ResponseEntity<Void> delete(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId,
             @PathVariable Long taskId
     ) {
-        taskWriteService.delete(todoId, taskId);
+        taskWriteService.delete(loginMember, todoId, taskId);
         return ResponseEntity.noContent().build();
     }
 }
