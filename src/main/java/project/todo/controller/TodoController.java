e9179085018a270a4083af9e26da1b04bb91fd87
youngrok <yr951103@gmail.com>
2025-02-10 14:39:11 +0900

    feat: TodoController - @Login 어노테이션으로 로그인 사용자 정보 주입

--- src/main/java/project/todo/controller/TodoController.java
+++ src/main/java/project/todo/controller/TodoController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.todo.model.todo.Status;
+import project.todo.service.member.Login;
+import project.todo.service.security.dto.LoginMember;
 import project.todo.service.todo.TodoReadService;
 import project.todo.service.todo.TodoWriteService;
 import project.todo.service.todo.dto.TodoCreateRequest;
@@ -24,51 +26,57 @@ public class TodoController {
 
     @GetMapping
     public ResponseEntity<List<TodoResponse>> findTodos(
+            @Login LoginMember loginMember,
             @RequestParam(required = false) Optional<Status> status
     ) {
-        var todos = todoReadService.findTodos(status);
+        var todos = todoReadService.findTodos(loginMember, status);
         return ResponseEntity.ok(todos);
     }
 
     @GetMapping("/{todoId}")
     public ResponseEntity<TodoWithTasksResponse> getTodoWithTasks(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId
     ) {
-        var todoWithTasks = todoReadService.getTodoWithTasks(todoId);
+        var todoWithTasks = todoReadService.getTodoWithTasks(loginMember, todoId);
         return ResponseEntity.ok(todoWithTasks);
     }
 
     @PostMapping
     public ResponseEntity<Void> create(
+            @Login LoginMember loginMember,
             @RequestBody TodoCreateRequest request
     ) {
-        todoWriteService.create(request);
+        todoWriteService.create(loginMember, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{todoId}")
     public ResponseEntity<Void> update(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId,
             @RequestBody TodoUpdateRequest request
     ) {
-        todoWriteService.update(todoId, request);
+        todoWriteService.update(loginMember, todoId, request);
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/{todoId}/status")
+    @PatchMapping("/{todoId}")
     public ResponseEntity<Void> updateStatus(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId,
             @RequestParam Status status
     ) {
-        todoWriteService.updateStatus(todoId, status);
+        todoWriteService.updateStatus(loginMember, todoId, status);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{todoId}")
     public ResponseEntity<Void> delete(
+            @Login LoginMember loginMember,
             @PathVariable Long todoId
     ) {
-        todoWriteService.delete(todoId);
+        todoWriteService.delete(loginMember, todoId);
         return ResponseEntity.noContent().build();
     }
 }
