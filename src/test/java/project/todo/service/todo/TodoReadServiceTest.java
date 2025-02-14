55860cdf6cb5789bf4231e0d5b106d9a69d412ce
youngrok <yr951103@gmail.com>
2025-02-14 17:32:56 +0900

    test: TodoReadService 유즈케이스 중심 테스트 케이스 보강 (로그인 사용자 적용)

--- src/test/java/project/todo/service/todo/TodoReadServiceTest.java
+++ src/test/java/project/todo/service/todo/TodoReadServiceTest.java
@@ -7,14 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import project.todo.model.member.Member;
+import project.todo.model.todo.Status;
 import project.todo.model.todo.Todo;
 import project.todo.model.todo.task.Task;
 import project.todo.repository.member.MemberRepository;
 import project.todo.repository.todo.TodoRepository;
 import project.todo.repository.todo.task.TaskRepository;
+import project.todo.service.security.dto.LoginMember;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,37 +37,54 @@ class TodoReadServiceTest {
     @Autowired
     private TodoReadService todoReadService;
 
+    private static final LoginMember LOGIN_MEMBER = new LoginMember(0L, "사용자");
+
     @BeforeEach
     void setUp() {
-        var member = new Member("user");
+        var member = new Member(
+                "사용자",
+                "loginId",
+                "password123",
+                "test@example.com"
+        );
         memberRepository.save(member);
 
         var todos = List.of(
                 new Todo(
-                        member.getId(),
+                        0L,
                         "todo1",
-                        LocalDate.of(2025, 9, 1)
+                        LocalDate.of(2030, 9, 1)
                 ),
                 new Todo(
-                        member.getId(),
+                        0L,
                         "todo2",
-                        LocalDate.of(2025, 10, 11)
+                        LocalDate.of(2030, 10, 11)
                 ),
                 new Todo(
-                        member.getId(),
+                        0L,
                         "todo3",
-                        LocalDate.of(2025, 11, 27)
+                        LocalDate.of(2030, 11, 27)
                 )
         );
         todoRepository.saveAll(todos);
+
+        var savedFirstTodo = todoRepository.findAll().get(0);
+        var tasks = List.of(
+                new Task(savedFirstTodo, "task1"),
+                new Task(savedFirstTodo, "task2"),
+                new Task(savedFirstTodo, "task3")
+        );
+        taskRepository.saveAll(tasks);
     }
 
     @DisplayName("사용자가 작성한 전체 Todo를 조회할 수 있다.")
     @Test
     void findTodos() {
-        var member = memberRepository.findAll().get(0);
+        var todo = todoRepository.findAll().get(0);
+        todo.complete();
+        Optional<Status> emptyStatus = Optional.empty();
 
-        var todos = todoReadService.findTodos(member.getId());
+        var todos = todoReadService.findTodos(LOGIN_MEMBER, emptyStatus);
 
         assertThat(todos).hasSize(3);
     }
@@ -72,22 +92,22 @@ class TodoReadServiceTest {
     @DisplayName("사용자가 작성한 Todo 중 완료된 Todo만 조회할 수 있다.")
     @Test
     void findCompleteTodos() {
-        var todo1 = todoRepository.findAll().get(0);
-        todo1.complete();
-        todoRepository.save(todo1);
+        var todo = todoRepository.findAll().get(0);
+        todo.complete();
+        Optional<Status> completeStatus = Optional.of(Status.COMPLETE);
 
-        var completeTodos = todoReadService.findCompleteTodos(todo1.getMemberId());
+        var todos = todoReadService.findTodos(LOGIN_MEMBER, completeStatus);
 
-        assertThat(completeTodos).hasSize(1);
-        assertThat(completeTodos.get(0).status().isCompleted()).isTrue();
+        assertThat(todos).hasSize(1);
+        assertThat(todos.get(0).status()).isEqualTo(Status.COMPLETE);
     }
 
     @DisplayName("완료된 Todo 조회 시 완료된 Todo가 없으면 빈 리스트를 반환한다.")
     @Test
     void findCompleteTodosWithEmpty() {
-        var member = memberRepository.findAll().get(0);
+        Optional<Status> completeStatus = Optional.of(Status.COMPLETE);
 
-        var completeTodos = todoReadService.findCompleteTodos(member.getId());
+        var completeTodos = todoReadService.findTodos(LOGIN_MEMBER, completeStatus);
 
         assertThat(completeTodos).isEmpty();
     }
@@ -95,14 +115,11 @@ class TodoReadServiceTest {
     @DisplayName("사용자가 작성한 Todo 중 미완료된 Todo만 조회할 수 있다.")
     @Test
     void findIncompleteTodos() {
-        var todo1 = todoRepository.findAll().get(0);
-        todo1.complete();
-        todoRepository.save(todo1);
+        Optional<Status> incompleteStatus = Optional.of(Status.INCOMPLETE);
 
-        var incompleteTodos = todoReadService.findIncompleteTodos(todo1.getMemberId());
+        var todos = todoReadService.findTodos(LOGIN_MEMBER, incompleteStatus);
 
-        assertThat(incompleteTodos).hasSize(2);
-        assertThat(incompleteTodos.get(0).status().isCompleted()).isFalse();
+        assertThat(todos).hasSize(3);
     }
 
     @DisplayName("미완료된 Todo 조회 시 미완료된 Todo가 없으면 빈 리스트를 반환한다.")
@@ -110,10 +127,9 @@ class TodoReadServiceTest {
     void findIncompleteTodosWithEmpty() {
         var todos = todoRepository.findAll();
         todos.forEach(Todo::complete);
-        todoRepository.saveAll(todos);
-        var member = memberRepository.findAll().get(0);
+        Optional<Status> incompleteStatus = Optional.of(Status.INCOMPLETE);
 
-        var incompleteTodos = todoReadService.findIncompleteTodos(member.getId());
+        var incompleteTodos = todoReadService.findTodos(LOGIN_MEMBER, incompleteStatus);
 
         assertThat(incompleteTodos).isEmpty();
     }
@@ -122,15 +138,7 @@ class TodoReadServiceTest {
     @Test
     void getTodoDetail() {
         var todo = todoRepository.findAll().get(0);
-        taskRepository.saveAll(
-                List.of(
-                        new Task(todo, "task1"),
-                        new Task(todo, "task2"),
-                        new Task(todo, "task3")
-                )
-        );
-
-        var getTodoWithTasks = todoReadService.getTodoWithTasks(todo.getId());
+        var getTodoWithTasks = todoReadService.getTodoWithTasks(LOGIN_MEMBER, todo.getId());
 
         assertThat(getTodoWithTasks.todoId()).isEqualTo(todo.getId());
         assertThat(getTodoWithTasks.tasks()).hasSize(3);
